6f8bf608513864ef1465ead356fd0087d47b1c5b
dfldiego <dfl.diego92@gmail.com>
2020-10-22 18:47:13 -0300

    middleware personalizado para validar campos.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,6 +3,7 @@
  */
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const usuarioController = require('../controllers/usuariosController');
 const router = Router();
 
@@ -11,6 +12,7 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
+    validarCampos,
 ], usuarioController.createUsuarios);
 
 module.exports = router;
