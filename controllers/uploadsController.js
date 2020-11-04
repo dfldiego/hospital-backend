6a86a6f2feaf231118e80827b907a68aabbda691
dfldiego <dfl.diego92@gmail.com>
2020-11-04 13:06:26 -0300

    Subir archivo. Instalacion de express-fileupload y uuid. ruta y controlador fileUpload y creamos de Upload folder

---
+++ controllers/uploadsController.js
@@ -0,0 +1,64 @@
+const { response } = require("express");
+const { v4: uuidv4 } = require('uuid');
+
+exports.fileUpload = (req, res = response) => {
+
+    const tipo = req.params.tipo;
+    const id = req.params.id;
+
+    // validar tipo - respecto a carpetas del uploads
+    const tiposValidos = ['hospitales', 'medicos', 'usuarios'];
+    if (!tiposValidos.includes(tipo)) {
+        return res.status(400).json({
+            ok: true,
+            msg: 'No es un medico, hospital o usuario (tipo)'
+        });
+    }
+
+    // validar que exista un archivo
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'No hay ningun archivo'
+        });
+    }
+
+    // Procesar la imagen.
+    //imagen es el nombre que viene en el KEY de POSTMAN.
+    const file = req.files.imagen;
+
+    // generar o extraer la extension del archivo
+    const nombreCortado = file.name.split('.');
+    const extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+    // validar extension
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    if (!extensionesValidas.includes(extensionArchivo)) {
+        return res.status(400).json({
+            ok: true,
+            msg: 'No es una extension permitida'
+        });
+    }
+
+    // generar el nombre del archivo
+    const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
+
+    // path para guardar la imagen
+    const path = `./uploads/${tipo}/${nombreArchivo}`;
+
+    // mover la imagen
+    file.mv(path, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error al mover la imagen'
+            });
+        }
+        res.json({
+            ok: true,
+            msg: 'Archivo subido',
+            nombreArchivo
+        });
+    });
+}
---
+++ routes/uploads.js
@@ -0,0 +1,15 @@
+/**
+ * UPLOADS
+ * ruta: api/uploads/
+ */
+
+const { Router } = require('express');
+const expressFileUpload = require('express-fileupload');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const uploadsController = require('../controllers/uploadsController');
+const router = Router();
+
+router.use(expressFileUpload());
+router.put('/:tipo/:id', validarJWT, uploadsController.fileUpload);
+
+module.exports = router;
